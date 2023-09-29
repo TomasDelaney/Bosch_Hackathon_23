70b5b8fd4808eaefa6fc0d64c7fedf442c3a215a
TomasDelaney <endrei.tomi@gmail.com>
2023-09-29 20:03:10 +0200

    Standardization for data and

--- functional_layer/Car_controll_environment.py
+++ functional_layer/Car_controll_environment.py
@@ -1,7 +1,10 @@
 import numpy as np
+import random
+import torch
 import gym
 from gym.utils import seeding
 from gym import spaces
+from preprocess_for_RL import load_csv
 
 
 class AutoEmergencyBreakEnv(gym.Env):
@@ -10,11 +13,36 @@ class AutoEmergencyBreakEnv(gym.Env):
 
     def __init__(self, max_acceleration, max_deceleration, max_wheel_turn_angle):
         super(AutoEmergencyBreakEnv, self).__init__()
+
         # car attributes
+        self.max_acc = max_acceleration
+        self.max_dec = max_deceleration
+        self.max_wheel_turn_angle = max_wheel_turn_angle
 
         # measured attributes from the camera
+        self.processed_data = load_csv()
 
         # actual values accounting for the RL agents actions
+        self.episode_data = self.processed_data
+
+        # reinforcement learning environment definition
+        self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(2,), dtype=np.float32)
+
+        low = np.array([
+            min(self.processed_data[""]),
+
+        ])
+
+        high = np.array([
+            max(self.processed_data[""]),
+
+        ])
+
+        self.observation_space = spaces.Box(low=low, high=high, dtype=np.float64)
+
+        self.state = self.init_training_start()
+
+        # PID controller values
 
     def init_training_start(self):
         # loads the csv and initialises the state vector
@@ -24,12 +52,27 @@ class AutoEmergencyBreakEnv(gym.Env):
         # transform the actions created by the agent: wheel angle and gas/break
         pass
 
-    def set_seed(self):
+    def set_seed(self, seed):
         # seed setting for reproducibility’s sake
+        torch.manual_seed(seed)  # Sets seed for PyTorch RNG
+        torch.cuda.manual_seed_all(seed)  # Sets seeds of GPU RNG
+        np.random.seed(seed)  # Set seed for NumPy RNG
+        random.seed(seed)  # Set seed for random RNG
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed(seed)
+            torch.cuda.manual_seed_all(seed)
+        print("Seed successfully set")
+
+    def step(self, action):
+        # take a step in the simulation
         pass
 
     def reset(self):
         # resets the environment
+        pass
+
+    def close(self):
+        # close the simulation
         pass
 
 # low level controller: PID
--- functional_layer/preprocess_for_RL.py
+++ functional_layer/preprocess_for_RL.py
@@ -11,6 +11,29 @@ def load_csv():
     # Convert DataFrame to a dictionary of NumPy arrays
     data_dict = {column: df[column].to_numpy() for column in df.columns}
 
+    # transform the distance data
+    distance_constant = 128
+    data_dict["FirstObjectDistance_X"] = data_dict["FirstObjectDistance_X"] / distance_constant
+    data_dict["FirstObjectDistance_Y"] = data_dict["FirstObjectDistance_Y"] / distance_constant
+    data_dict["SecondObjectDistance_X"] = data_dict["SecondObjectDistance_X"] / distance_constant
+    data_dict["SecondObjectDistance_Y"] = data_dict["SecondObjectDistance_Y"] / distance_constant
+    data_dict["ThirdObjectDistance_X"] = data_dict["ThirdObjectDistance_X"] / distance_constant
+    data_dict["ThirdObjectDistance_Y"] = data_dict["ThirdObjectDistance_Y"] / distance_constant
+    data_dict["FourthObjectDistance_X"] = data_dict["FourthObjectDistance_X"] / distance_constant
+    data_dict["FourthObjectDistance_Y"] = data_dict["FourthObjectDistance_Y"] / distance_constant
+
+    # transform the velocity data
+    velocity_constant = 256
+    data_dict["VehicleSpeed"] = data_dict["VehicleSpeed"] / velocity_constant
+    data_dict["FirstObjectSpeed_X"] = data_dict["FirstObjectSpeed_X"] / velocity_constant
+    data_dict["FirstObjectSpeed_Y"] = data_dict["FirstObjectSpeed_Y"] / velocity_constant
+    data_dict["SecondObjectSpeed_X"] = data_dict["SecondObjectSpeed_X"] / velocity_constant
+    data_dict["SecondObjectSpeed_Y"] = data_dict["SecondObjectSpeed_Y"] / velocity_constant
+    data_dict["ThirdObjectSpeed_X"] = data_dict["ThirdObjectSpeed_X"] / velocity_constant
+    data_dict["ThirdObjectSpeed_Y"] = data_dict["ThirdObjectSpeed_Y"] / velocity_constant
+    data_dict["FourthObjectSpeed_X"] = data_dict["FourthObjectSpeed_X"] / velocity_constant
+    data_dict["FirstObjectSpeed_Y"] = data_dict["FirstObjectSpeed_Y"] / velocity_constant
+
     # Now, data_dict is a dictionary where keys are column names and values are NumPy arrays
     return data_dict
 
