0822158e3a674a02a4c77fc486e7bd089710d6fb
zelere <88401726+zelere@users.noreply.github.com>
2023-09-29 16:12:26 +0200

    Update Break_distance_bosch_calc.py

--- Break_distance_bosch_calc.py
+++ Break_distance_bosch_calc.py
@@ -20,3 +20,29 @@ def calculate_brake_distance_bosch(vEgo, t_lat, aEgo, v0):
     break_distance = d1 + d2 + d3
 
     return break_distance
+
+'''## Break Distance calculation
+The AASHTO stopping distance formula is as follows:
+
+s = (0.278 × t × v) + v² / (254 × (f + G))
+
+where:
+
+s – Stopping distance in meters;
+t – Perception-reaction time in seconds;
+v – Speed of the car in km/h;
+G – Grade (slope) of the road, expressed as a decimal. Positive for an uphill grade and negative for a downhill road; and
+f – Coefficient of friction between the tires and the road. It is assumed to be 0.7 on a dry road and between 0.3 and 0.4 on a wet road.
+This formula is taken from the book "A Policy on Geometric Design of Highways and Streets". It is commonly used in road design for establishing the minimum stopping sight distance required on a given road. With correct parameters, it's a perfect equation for the accurate calculation of the stopping distance of your car. Clearly, it's different than the typical formula used in the speed calculator.
+
+[https://www.omnicalculator.com/physics/stopping-distance](https://www.omnicalculator.com/physics/stopping-distance)'''
+
+
+def calculate_brake_distance_2(t_reaction, v_kmh):
+    G = 0  #Grade(slope) of the road, expressed as a decimal.
+    # Positivefor an uphill grade and negative for a downhill road
+    f = 0.7     #Coefficient of friction between the tires and the road. 
+    # It is assumed to be 0.7 on a dry road and between 0.3 and 0.4 on a wet road.
+    break_dist = ((0.278 * t_reaction * v_kmh) + v_kmh*v_kmh) / (254 * (f + G))
+    
+    return break_dist
