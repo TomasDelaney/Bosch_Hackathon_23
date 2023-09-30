ee3c74a64e551e4193c853cc1cc9c50db6ede8ab
zelere <88401726+zelere@users.noreply.github.com>
2023-09-30 13:01:28 +0200

    Update objectFilter.py

--- objectFilter.py
+++ objectFilter.py
@@ -1,9 +1,9 @@
 import math
-import breakdist
 
 
 def calcNewLoc(x, y, Speed_X, Speed_Y, t, dt, yaw, v):
-    alpha = - yaw * dt
+    '''Calculate future locations of object based on the current properties'''
+    alpha = - yaw * t
     x = x - (v - Speed_X) * t
     y = y + Speed_Y * t
     new_x = math.cos(alpha) * x - math.sin(alpha) * y
@@ -11,47 +11,72 @@ def calcNewLoc(x, y, Speed_X, Speed_Y, t, dt, yaw, v):
     return {'x': new_x, 'y': new_y}
 
 
+def filterClosest(situation):
+    '''Filter objects based on distance
+        return value: index of closest object'''
+
+    p_dist = []  # distance container
+    midx = -100;  # index of the relevant object initialization
+    for i, obj in enumerate(situation['objects']):
+        if obj['Distance_X'] != 0:  # we don't need any data which is 0
+            dist = math.sqrt(math.pow(obj['Distance_Y'], 2) + math.pow(obj['Distance_X'], 2))
+            p_dist.append(dist)  # add dist to a list
+        else:
+            p_dist.append(9999)  # if we have 0 x than we give it a big distance, because it a measure error
+        midx = p_dist.index(min(p_dist))  # get the index of the min distance
+    situation['objects'][midx]['state'] = True  # set it to the relevant objext
+
+    # print(situation['objects'][midx]['name'], situation['objects'][midx]['state'], 'close')
+
+    return midx
+
+
 def predictPath(situation, time_range, break_dist, dt, CAR_WIDTH):
+
     closest = {'x': 100000, 'y': 1000000, 't': 1000000, 'name': ''}
-    for obj in situation['objects']:
+    for idx, obj in enumerate(situation['objects']):
         done = False
         for t in range(0, time_range * 100, 1):
             t = float(t) / 100
 
             if not done:
-                if not (obj['Distance_X'] == 0 and obj['Distance_Y'] == 0):
+                if not (obj['Distance_X'] == 0): # if tracking data exists
+                    #calculate
                     loc = calcNewLoc(x=obj['Distance_X'], y=obj['Distance_Y'],
                                      Speed_X=obj['Speed_X'], Speed_Y=obj['Speed_Y'],
                                      t=t, dt=dt, yaw=situation["YawRate"], v=situation['VehicleSpeed'])
 
                     if abs(loc['y']) < CAR_WIDTH / 2 and loc['x'] < break_dist * 1.5:
-                        done = True
+                        '''if future location is in front of the car 
+                            and predicted distance is under 1.5 of breakdistance '''
                         if closest['t'] > t:
                             closest['x'] = obj['Distance_X']
                             closest['y'] = obj['Distance_Y']
                             closest['t'] = t
-                            closest['Speed_x'] = obj['Speed_X']
+                            closest['Speed_X'] = obj['Speed_X']
                             closest['Speed_Y'] = obj['Speed_Y']
                             closest['name'] = obj['name']
-
-                        print(
-                            f"Break needed in {t} seconds on object {obj['name']} obj location {obj['Distance_X']}  {obj['Distance_Y']}")
-
-                        print(
-                            f"Break needed in {t} seconds on object {obj['name']} obj location {obj['Distance_X']}  {obj['Distance_Y']}")
+                            closest['idx'] = idx
     if closest['name'] != '':
         return closest
 
 
 def warning(closest):
-    if closest['t'] < 1.5:
-        print("*** Flashig lights and sound effects ***")
-        return [closest['Speed_X'], closest['Speed_Y']]
-    elif closest['t'] < 3:
+    if 1.5 <= closest['t'] < 3:
+        print("*** Tactile WARNING ***", closest['name'])
+        print(f"Possible collision {closest['t']} seconds to notice", closest['name'])
+
+        return {'warning': 'Tactile warning', 'x': closest['Speed_X'], 'y': closest['Speed_Y']}
+    elif closest['t'] < 1.5:
         print("*** Tactile WARNING ***")
-        return [closest['Speed_X'], closest['Speed_Y']]
+        print("*** Flashig lights and horn effects ***", closest['name'])
+        print(f"Possible collision {closest['t']} seconds to notice", closest['name'])
+
+        return {'warning': 'Tactile & sound warning', 'x': closest['Speed_X'], 'y': closest['Speed_Y']}
+    elif closest['t'] < 0.2:
+        print("*** EMERGENCY BREAK ***", closest['name'])
+        print(f"Possible collision {closest['t']} seconds to notice", closest['name'])
+        return {'warning': 'Emergency break', 'x': closest['Speed_X'], 'y': closest['Speed_Y']}
     else:
-        print(f"Possible collision but still {closest['t']} seconds to notice")
-        return []
-
-
+        print(f"Possible collision danger {closest['t']} seconds to notice", closest['name'])
+        return {'warning': 'Not dangerous'}
